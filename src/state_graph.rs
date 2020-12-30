914632d0578b97cbdefe7054448a4baf1a3334ae
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-12-30 08:14:12 -0500

    initial Cargo organization

---
+++ src/state_graph.rs
@@ -0,0 +1,7 @@
+/*
+    The core data structure for tracking live/dead states
+    in an abstract transition system.
+
+    Parallels state_graph.cpp in the Z3 code base -- found in
+        z3/src/util/state_graph.h.
+*/
