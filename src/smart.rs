4ce338f4fb73cf93bf6dd7f76d43f00e6b357ce2
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-01-02 21:50:03 -0500

    placeholders for future implementations of the StateGraph interface

---
+++ src/smart.rs
@@ -0,0 +1,7 @@
+/*
+    Our new implementation of the StateGraph trait.
+    Uses "jump" pointers to jump from each state a large number
+    of states ahead at once.
+
+    TODO (Planned)
+*/
---
+++ src/tarjan.rs
@@ -0,0 +1,7 @@
+/*
+    Implementation of the StateGraph trait using Tarjan's
+    algorithm for online strongly-connected-components (the state of the art
+    in terms of computational complexity).
+
+    TODO (Planned)
+*/
