69696acd5ee79450e096242af5b9a150dcb8e2c1
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-01-02 19:42:01 -0500

    minor improvements to the StateGraph trait interface

--- src/interface.rs
+++ src/interface.rs
@@ -28,27 +28,35 @@ pub struct ExampleOutput {
     unvisited: Vec<usize>,
 }
 
-pub trait StateGraph {
+pub trait StateGraph: Sized {
     /*
         Functions that need to be implemented.
 
-        For convenience, these versions are unchecked:
+        For convenience, the main functions are unchecked:
         - add_transition_unchecked can assume both its vertices are distinct and that the
           source is Unvisited.
         - mark_done_unchecked can assume that its vertex is Unvisited.
+
+        Derived checked versions are then provided as safe wrappers around these.
     */
+
     // Constructor
     fn new() -> Self;
+
     // Add a new transition to the graph from an Unvisited state to any state.
     // (If the vertex doesn't exist yet, create it and mark it unvisited.)
     fn add_transition_unchecked(&mut self, v1: usize, v2: usize);
+
     // Mark an unvisited state as visited.
     fn mark_done_unchecked(&mut self, v: usize);
+
     // Return whether v is Unvisited, or v is Visited but there is a path from
     // v to an Unvisited state (Unknown), or there is no such path (Dead).
     fn get_status(&self, v: usize) -> Status;
+
     // Return a vector of all states that have been seen.
     fn vec_states(&self) -> Vec<usize>;
+
     // Statistics -- only work in debug mode
     // space should be true memory, up to a constant, and time should be true
     // time, up to a constant.
@@ -57,10 +65,13 @@ pub trait StateGraph {
     fn get_space(&self) -> usize;
     fn get_time(&self) -> usize;
 
-    // Safe, checked versions
-    // These should always be used over the unchecked versions for code that
-    // uses the interface naively.
-    // TODO: If this becomes a real library, return Result instead of panicking.
+    /*
+        Derived (default) functions
+    */
+
+    // The safe add_transition and mark_done should generally be used
+    // over the unchecked versions as they validate that the sequence of
+    // inputs is correct.
     fn add_transition(&mut self, v1: usize, v2: usize) {
         assert!(self.get_status(v1) == Status::Unvisited);
         if v1 != v2 {
@@ -73,12 +84,6 @@ pub trait StateGraph {
     }
 
     // Same as the above but using the Transaction enum
-    fn process_unchecked(&mut self, t: Transaction) {
-        match t {
-            Transaction::Add(v1, v2) => self.add_transition_unchecked(v1, v2),
-            Transaction::Done(v1) => self.mark_done_unchecked(v1),
-        }
-    }
     fn process(&mut self, t: Transaction) {
         match t {
             Transaction::Add(v1, v2) => self.add_transition(v1, v2),
@@ -109,10 +114,9 @@ pub trait StateGraph {
         unvisited.sort_unstable();
         ExampleOutput { dead, unknown, unvisited }
     }
-    fn run_example(input: &ExampleInput) -> ExampleOutput
-    where
-        Self: Sized,
-    {
+
+    // Top-level function to run an input and produce an output
+    fn run_example(input: &ExampleInput) -> ExampleOutput {
         let mut state_graph = Self::new();
         state_graph.process_all(input);
         state_graph.collect_all()
