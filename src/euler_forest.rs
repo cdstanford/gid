af313c43ff5824bab93ebad197bf605b0fc84228
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-05-28 09:17:33 -0400

    revert to HashMap-backed AVL, performs better
    (corrects timing-out unit tests and crashing --ignored)

--- src/euler_forest.rs
+++ src/euler_forest.rs
@@ -40,7 +40,7 @@
        http://courses.csail.mit.edu/6.851/spring07/scribe/lec05.pdf
 */
 
-use super::avl_forest::AvlForest2DVec;
+use super::avl_forest::AvlForestHM;
 use std::fmt::Debug;
 
 // For this file, we use usize to identify vertices.
@@ -66,7 +66,7 @@ fn vert_id(v: IdType) -> NodeId {
 */
 #[derive(Debug, Default)]
 pub struct EulerForest {
-    nodes: AvlForest2DVec,
+    nodes: AvlForestHM<NodeId>,
 }
 impl EulerForest {
     pub fn new() -> Self {
