5d2f2cbc83be6995c2ea1d51db147ce83251e77e
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-05-30 10:50:31 -0400

    Revert "random examples size 3 (for completeness)"
    This reverts commit 67608e15869f1378ee6166a9fc8f3d3278e62168.

--- src/bin/example_gen.rs
+++ src/bin/example_gen.rs
@@ -241,14 +241,14 @@ fn main() {
     }
     // Generate and save random examples
     // Use random seeds 1-10
-    for &n in &[3, 10, 30, 100, 300, 1000, 3000, 10000] {
+    for &n in &[10, 30, 100, 300, 1000, 3000, 10000] {
         for &d in &[1, 2, 3, 10] {
             for seed in 1..=10 {
                 random_sparse(n, d, seed).save();
             }
         }
     }
-    for &n in &[3, 10, 30, 100, 300, 1000, 3000] {
+    for &n in &[10, 30, 100, 300, 1000, 3000] {
         for &d in &[1, 2, 3] {
             for seed in 1..=10 {
                 random_dense(n, d, seed).save();
